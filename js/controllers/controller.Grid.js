1993b01812207245c44e3368243696d45aaae111
Eric <info@ericparlier.com>
2019-10-28 22:40:44 -0400

    oml render in GridSwiper

--- js/controllers/controller.Grid.js
+++ js/controllers/controller.Grid.js
@@ -2,19 +2,21 @@ require('Controllers.Grid', 'Controller');
 OGX.Controllers.Grid = function(__data){
     construct(this, 'Controllers.Grid');
     'use strict';  
+    var oml = JSON.parse('{"default:Views.Views":{}}');
     var grid;  
 
     //@Override
     this.construct = function(){       
-       grid = app.getStage().find(OGX.GridSwiper.NAME, 'grid');
-       grid.setMap([
-           [displayPoint, displayPoint, displayPoint],
-           [displayPoint, displayPoint, displayPoint],
-           [displayPoint, displayPoint, displayPoint]
+        
+        grid = app.getStage().find(OGX.GridSwiper.NAME, 'grid');
+        grid.setMap([
+            [displayPoint, oml, displayPoint],
+            [displayPoint, displayPoint, displayPoint],
+            [displayPoint, displayPoint, displayPoint]
         ]);
     };
 
-    function displayPoint(__abs, __rel){
+    function displayPoint(__abs, __rel){        
         var color = [50+Math.round(Math.random()*100), 50+Math.round(Math.random()*100), 50+Math.round(Math.random()*100)];
         return '<div style="display:block; width:100%; height:100%; background-color:rgb('+color.join(',') +'")><span class="msg">Relative Point: '+JSON.stringify(__rel)+'<br>Absolute Point: '+JSON.stringify(__abs)+'</span></div>';
     }
