77266e388c5672f7a7636d2e6ddcc4363ae7d6d8
Eric <info@ericparlier.com>
2019-10-30 20:53:18 -0400

    controller

---
+++ js/controllers/controller.Chat.js
@@ -0,0 +1,18 @@
+require('Controllers.Chat', 'Controller');
+OGX.Controllers.Chat = function(){
+    construct(this, 'Controllers.Chat');
+    'use strict';  
+    let chat;
+
+    //@Override  
+    this.construct = function(){
+        chat = app.getStage().find(OGX.Chat.NAME, 'chat');
+        chat.users([{_id:1031, first_name:'Globules', last_name:'Robot'}, {_id:1032, first_name:'Your', last_name:'Name'}]);
+        chat.user(1032);
+        chat.addMessage({from:1031, body:'Hi! Welcome to OGX.JS! We\'re not available right now but leave a message!', date:moment()});
+    };  
+
+    //@Override  
+    this.ux = function(__bool){};     
+
+};
