84569a26eeb3495454181b35f6e1d6e0303b00ce
Eric <info@ericparlier.com>
2019-10-30 20:47:53 -0400

    view

---
+++ js/views/view.PopupAdvanced.js
@@ -0,0 +1,87 @@
+/*
+@View
+@Desc: Used for other Popup demos BUT standard
+*/
+require('Views.PopupAdvanced', 'View');
+OGX.Views.PopupAdvanced = function(__config){
+    construct(this, 'Views.PopupAdvanced');
+	'use strict';
+	let that = this;   
+    let config = __config;
+    let popup;
+
+    //@Override   
+    this.construct = function(){
+        let conf = genConf();
+        if(!conf){
+            popup = this.find(OGX.Popup.NAME, 'container');
+        }else{
+            popup = this.create('Popup', conf);     
+        }             
+        popup.show(OGX.Popup.FADE); 
+        OGX.Audio.playSound('click.mp3', 0.1);
+    };  
+    
+    this.destroy = function(){
+        $('#instance').css('background-color', 'unset');
+    }; 
+
+    function genConf(){
+        let conf = {
+            id:'popup',
+            width:220,            
+            drag:true                
+        };
+        switch(config.mode){ 
+            case 'advanced':  
+            conf.title = 'Resize & Drag';
+            conf.html = '<span class="popup_message_center">Advanced Popup</span>';
+            conf.resize = true;
+            conf.height = 200;            
+            break;   
+
+            case 'dialogs': 
+            conf.title = 'Confirm';
+            conf.html = '<span class="popup_message_dialogs">You are sure you want to do that?</span>';
+            conf.buttons = [{label:"YES", callback:onAction, params:0}, {label:"MAYBE", callback:onAction, params:1}]
+            conf.height = 180;   
+            break;
+
+            case 'container':            
+            return false;
+        }
+        return conf;
+    }
+
+    function onAction(__action){
+        popup.hide(OGX.Popup.FADE, function(){
+            that.remove(OGX.Popup.NAME, 'popup');
+            setTimeout(function(){
+                let conf = genConf();
+                conf.buttons = [{label:'OK', callback:onAction, params:2}];
+                switch(__action){
+                    case 0:
+                    conf.title = 'Done!';
+                    conf.html = '<span class="popup_message_dialogs">I mean done-ish, will finish at some point.</span>';   
+                    break;
+
+                    case 1:
+                    conf.title = 'Postponed!';
+                    conf.html = '<span class="popup_message_dialogs">Think about it then and come back later!</span>';   
+                    break;
+
+                    case 2:
+                    popup = that.create(OGX.Popup.NAME, genConf());  
+                    popup.show(OGX.Popup.FADE);  
+                    OGX.Audio.playSound('click.mp3', 0.1);    
+                    return;                    
+                }  
+                popup = that.create(OGX.Popup.NAME, conf);           
+                popup.show(OGX.Popup.FADE);
+                OGX.Audio.playSound('click.mp3', 0.1);
+            }, 1500)}
+        );       
+    }
+   
+    $('#instance').css('background-color', 'cadetblue');
+};
