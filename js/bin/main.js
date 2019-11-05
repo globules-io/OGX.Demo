e556b69a9efb48a5c2be419a7396209cbc38d8c5
Eric <info@ericparlier.com>
2019-11-04 19:41:58 -0500

    js bin

---
+++ js/bin/main.js
@@ -0,0 +1,13 @@
+/*APP*/  
+var app; 	
+$(document).ready(function(){		
+    $(document).on(OGX.App.READY, function(){
+        $(document).off(OGX.App.READY); 
+        $('.ogx_overlay').remove();       
+    });
+    app = new OGX.App();     
+});
+/*GLOBALS*/
+function exists(__var){
+    return typeof(__var) !== 'undefined';
+}
